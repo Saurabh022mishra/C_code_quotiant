1ee444e6017ab4ad0dbf88b92b55f6f4c90e43e0
Saurabh022mishra <114359238+Saurabh022mishra@users.noreply.github.com>
2023-03-22 06:34:20 +0000

    Arithmetic_operators

---
+++ Arithmatic_operators.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int a,b,c,d;
+    a=5;
+    b=2;
+    c=1;
+    d=2;
+    printf("a=%d b=%d c=%d d=%d\n\n",a,b,c,d);
+    printf("a+b=%d\n",a+b);
+    printf("a-b=%d\n",a-b);
+    printf("a*b=%d\n",a*b);
+    printf("a/b=%d\n",a/b);
+    printf("a%%b=%d\n\n",a%b);
+    printf("c+d=%d\n",c+d);
+    printf("c-d=%d\n",c-d);
+    printf("c/d=%d\n",c/d);
+    printf("c/d=%d\n",c/d);
+    printf("c%%d=%d\n",c%d);
+    return 0;
+}
